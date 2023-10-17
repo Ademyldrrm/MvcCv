628e25a5ec2f4295922a042de3797f7fefe1b543
Adem Yıldırım <yldrmadem52@gmail.com>
2023-10-17 19:13:39 +0300

    first commit

---
+++ MvcCv/Controllers/ExperienceController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Repositories;
+
+namespace MvcCv.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        // GET: Experience
+        ExperienceRepository repo = new ExperienceRepository();
+
+        public ActionResult Index()
+        {
+            var values = repo.List();
+            return View(values);
+        }
+    }
+}
